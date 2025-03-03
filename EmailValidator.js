24b05483639fca88cbb775bcd5b952f11c00ee0b
Rajveer188 <veerkajle188@gmail.com>
2025-03-04 00:08:45 +0530

    [Rajveer] Add. Ensure . after domain and validate 3 mandormandatory 3rd part i.e. co

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,5 +1,5 @@
 //regex to validate email
-const regex = /^[a-zA-Z0-9]+/;
+const regex = /^[a-zA-Z0-9]+@[a-zA-z]+\.[a-zA-Z]+/;
 
 //function to validate email
 function validate(email){
@@ -8,4 +8,7 @@ function validate(email){
 
 //test
 console.log(validate("abc"));
+console.log(validate("abc@bridgelabz"));
+console.log(validate("abc@bridgelabz.co"));
+
 
