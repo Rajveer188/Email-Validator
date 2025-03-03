69e50864b29c86af0b32bf0389a3c0b9131d4c6c
Rajveer188 <veerkajle188@gmail.com>
2025-03-03 23:58:31 +0530

    [Rajveer] Add. validate username using regex

---
+++ EmailValidator.js
@@ -0,0 +1,11 @@
+//regex to validate email
+const regex = /^[a-zA-Z0-9]+/;
+
+//function to validate email
+function validate(email){
+    return regex.test(email)
+}
+
+//test
+console.log(validate("abc"));
+
